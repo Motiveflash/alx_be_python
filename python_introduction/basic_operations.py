b3c5aa020cc786c43723a9816576c854168d7149
Tawab Baah <tawabbaah35@gmail.com>
2024-09-10 12:13:57 +0000

    Initial, first committed the python basic_operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#defined the variables
+number1 = 10 # assigned 10 to nuber1
+number2 = 5 # assigned 5 to number2
+
+#calculating the sum, difference, and product, Print 
+result = number1 + number2 #Addition
+print(f"Addition of {number1} and {number2} is {result}")
+
+result = number1 - number2 #Subtrction
+print(f"Subtraction of {number1} and {number2} is {result}")
+
+result = number1 * number2 #Multiplication 
+print(f"Multiplication of {number1} and {number2} is {result}")
