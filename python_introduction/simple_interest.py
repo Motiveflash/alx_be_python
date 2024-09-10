824fef8c4a78ccd52c3196e6ae283b19260629a8
Tawab Baah <tawabbaah35@gmail.com>
2024-09-10 12:42:42 +0000

    Added a programe that calculate the simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+#This program calculates the simple interest
+principal = 1000 #initial investment of &1000
+rate = 0.05 # 5% annual interest rate
+time = 3 # 3 years
+
+#calculate the simple interest (I = P * R * T)
+interest = principal * rate * time
+print(f"The simple interest is: {interest}")
