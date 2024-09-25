142e0d88ccdad4f02000555f03c9f1dcc229d017
Tawab Baah <tawabbaah35@gmail.com>
2024-09-25 21:07:52 +0000

    handled number input error

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -9,39 +9,42 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = input("Enter your choice: ")
-
-        if choice == '1':
-            item = input("Enter item: ")
-            if item in shopping_list:
-                print(f"{item} already exist")
-                add = input("Would you like to keep it? Enter y for [y]es and n for [n]o ").lower()
-                if add == "y":
-                    shopping_list.append(item)
+        try:
+            choice = int(input("Enter your choice: "))
+        
+            if choice == '1':
+                item = input("Enter the item to add: ")
+                if item in shopping_list:
+                    print(f"{item} already exist")
+                    add = input("Would you like to keep it? (Y/N) ").lower()
+                    if add == "y":
+                        shopping_list.append(item)
+                    else:
+                        exit
                 else:
-                    exit
+                    shopping_list.append(item)
+            elif choice == '2':
+                item = input("Enter item: ")
+                if item not in shopping_list:
+                    print("Item dose not exist")
+                else:
+                    shopping_list.remove(item)
+                    print(f"{item} has been removed")
+            elif choice == '3':
+                if not shopping_list:
+                    print("The list is empty")
+                else:
+                    print("\nShopping list")
+                    for index, item in enumerate(shopping_list, start=1):
+                        index = "--->"
+                        print(f"{index}. {item.capitalize()}")
+            elif choice == '4':
+                print("Goodbye!")
+                break
             else:
-                shopping_list.append(item)
-        elif choice == '2':
-            item = input("Enter item: ")
-            if item not in shopping_list:
-                print("Item dose not exist")
-            else:
-                shopping_list.remove(item)
-                print(f"{item} has been removed")
-        elif choice == '3':
-            if not shopping_list:
-                print("The list is empty")
-            else:
-                print("\nShopping list")
-                for index, item in enumerate(shopping_list, start=1):
-                    index = "--->"
-                    print(f"{index}. {item.capitalize()}")
-        elif choice == '4':
-            print("Goodbye!")
-            break
-        else:
-            print("Invalid choice. Please try again.")
+                print("Invalid choice. Please try again.")
+        except ValueError:
+            print("choice is not a number")
 
 if __name__ == "__main__":
     main()
