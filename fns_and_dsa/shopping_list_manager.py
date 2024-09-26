bec5af8cd515dda80ff8d5aeab0a817c431acc68
Tawab Baah <tawabbaah35@gmail.com>
2024-09-26 14:33:18 +0000

    fixed choice and item error

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,33 +12,33 @@ def main():
         try:
             choice = int(input("Enter your choice: "))
         
-            if choice == '1':
+            if choice == 1:
                 item = input("Enter the item to add: ")
-                if item in shopping_list:
+                item_name = item.lower()
+                if item_name in shopping_list:
                     print(f"{item} already exist")
                     add = input("Would you like to keep it? (Y/N) ").lower()
                     if add == "y":
-                        shopping_list.append(item)
+                        shopping_list.append(item_name)
                     else:
                         exit
                 else:
-                    shopping_list.append(item)
-            elif choice == '2':
-                item = input("Enter item: ")
+                    shopping_list.append(item_name)
+            elif choice == 2:
+                item = input("Enter the item to remove: ")
                 if item not in shopping_list:
                     print("Item dose not exist")
                 else:
                     shopping_list.remove(item)
                     print(f"{item} has been removed")
-            elif choice == '3':
+            elif choice == 3:
                 if not shopping_list:
                     print("The list is empty")
                 else:
                     print("\nShopping list")
                     for index, item in enumerate(shopping_list, start=1):
-                        index = "--->"
                         print(f"{index}. {item.capitalize()}")
-            elif choice == '4':
+            elif choice == 4:
                 print("Goodbye!")
                 break
             else:
