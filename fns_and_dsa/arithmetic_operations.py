746c6645f6a5aac0bff0cea0a3d906a0b3bd4e5d
Tawab Baah <tawabbaah35@gmail.com>
2024-09-26 15:54:05 +0000

    modified with elif

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,33 @@
+# def perform_operation(num1, num2, operation):
+#   match operation:
+#     case 'add':
+#       return num1 + num2
+#     case 'subtract':
+#       return num1 - num2
+#     case 'multiply':
+#       return num1 * num2
+#     case 'divide':
+#       if num2 == 0:
+#         print("Zero(0) division error")
+#       else:
+#         return num1 / num2
+#     case _:
+#       print("Enter a valid operator")
+
+
 def perform_operation(num1, num2, operation):
-  match operation:
-    case 'add':
-      return num1 + num2
-    case 'subtract':
-      return num1 - num2
-    case 'multiply':
-      return num1 * num2
-    case 'divide':
-      if num2 == 0:
-        print("Zero(0) division error")
-      else:
-        return num1 / num2
-    case _:
-      print("Enter a valid operator")
+  if operation == 'add':
+    return num1 + num2
+  elif operation == 'subtract':
+    return num1 - num2
+  elif operation == 'multiply':
+    return num1 * num2
+  elif operation == 'divide':
+    if num2 == 0:
+      print("Zero(0) division error")
+    else:
+      return num1 / num2
+  else:
+    print("Enter a valid operator")
+
+
