d2cf4a5fbbec575e528ce79bed46e89004cbf773
Tawab Baah <tawabbaah35@gmail.com>
2024-10-02 16:11:10 +0000

    Debug bank account program

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,14 +6,11 @@ class BankAccount:
 
     def deposit(self, amount):
          self.account_balance += amount
-         print(f"Deposited: ${amount}")
+         return self.account_balance
 
     def withdraw(self, amount):
          if amount <= self.account_balance:
               self.account_balance -= amount
-              print(f"Withdrew: ${amount}")
-         else:
-              print("Insufficient funds.")
 
     def display_balance(self):
-         print(f"Current Balance: ${self.account_balance}")
+         return self.account_balance
