248e21727a7e0137370c5c4409044f99dc453c05
Tawab Baah <tawabbaah35@gmail.com>
2024-10-02 16:11:10 +0000

    Debug bank account and check for negative deposit

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,19 +1,20 @@
 # Class of bank acount
-
 class BankAccount:
     def __init__(self, initial_balance = 0):
-         self.account_balance = initial_balance
+        self.account_balance = initial_balance
 
     def deposit(self, amount):
-         self.account_balance += amount
-         print(f"Deposited: ${amount}")
+        if amount <= 0: # Check if deposit amount is less than or equal to zero(0)
+            print("Invalid amount, should be greater that zero(0)")
+        else:
+            self.account_balance += amount
 
     def withdraw(self, amount):
-         if amount <= self.account_balance:
-              self.account_balance -= amount
-              print(f"Withdrew: ${amount}")
-         else:
-              print("Insufficient funds.")
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
 
     def display_balance(self):
-         print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
